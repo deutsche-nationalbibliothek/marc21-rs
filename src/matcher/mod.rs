ad67d0634eafef664bf8a13818347312ba8ce91c
Nico Wagner <n.wagner@dnb.de>
2026-01-23 13:09:46 +0100

    Add indicator matcher (#15)
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- src/matcher/mod.rs
+++ src/matcher/mod.rs
@@ -1,5 +1,6 @@
 mod common;
 mod error;
+mod indicator_matcher;
 mod options;
 mod quantifier;
 mod subfield_matcher;
@@ -7,6 +8,7 @@ mod tag_matcher;
 mod value;
 
 pub use error::ParseMatcherError;
+pub use indicator_matcher::IndicatorMatcher;
 pub use options::MatcherOptions;
 pub use subfield_matcher::SubfieldMatcher;
 pub use tag_matcher::TagMatcher;
