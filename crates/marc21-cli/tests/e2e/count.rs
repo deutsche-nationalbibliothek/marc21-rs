04a98dc49714c1c7c830d52f7b69218c466e22f4
Nico Wagner <n.wagner@dnb.de>
2026-02-20 06:22:29 +0100

    Update `count` e2e tests (#86)
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- crates/marc21-cli/tests/e2e/count.rs
+++ crates/marc21-cli/tests/e2e/count.rs
@@ -6,10 +6,10 @@ use flate2::read::GzDecoder;
 use crate::prelude::*;
 
 #[test]
-fn count_default() -> TestResult {
+fn count_stdout() -> TestResult {
     let mut cmd = marc21_cmd();
     let assert =
-        cmd.args(["count"]).arg(data_dir().join("ada.mrc")).assert();
+        cmd.arg("count").arg(data_dir().join("ada.mrc")).assert();
 
     assert
         .success()
@@ -21,42 +21,42 @@ fn count_default() -> TestResult {
 }
 
 #[test]
-fn count_read_gzip() -> TestResult {
+fn count_gzip() -> TestResult {
     let mut cmd = marc21_cmd();
     let assert = cmd
-        .args(["count"])
-        .arg(data_dir().join("ada.mrc.gz"))
+        .args(["count", "-s"])
+        .arg(data_dir().join("DUMP.mrc.gz"))
         .assert();
 
     assert
         .success()
         .code(0)
-        .stdout(predicates::ord::eq("1\n"))
+        .stdout(predicates::ord::eq("7\n"))
         .stderr(predicates::str::is_empty());
 
     Ok(())
 }
 
 #[test]
-fn count_read_multiple_files() -> TestResult {
+fn count_multiple_files() -> TestResult {
     let mut cmd = marc21_cmd();
     let assert = cmd
-        .args(["count"])
-        .arg(data_dir().join("ada.mrc.gz"))
+        .args(["count", "-s"])
+        .arg(data_dir().join("DUMP.mrc.gz"))
         .arg(data_dir().join("ada.mrc"))
         .assert();
 
     assert
         .success()
         .code(0)
-        .stdout(predicates::ord::eq("2\n"))
+        .stdout(predicates::ord::eq("8\n"))
         .stderr(predicates::str::is_empty());
 
     Ok(())
 }
 
 #[test]
-fn count_write_output_text() -> TestResult {
+fn count_output() -> TestResult {
     let temp_dir = TempDir::new()?;
     let output = temp_dir.child("count.txt");
 
@@ -84,7 +84,7 @@ fn count_write_output_text() -> TestResult {
 }
 
 #[test]
-fn count_write_output_gzip() -> TestResult {
+fn count_output_gzip() -> TestResult {
     let temp_dir = TempDir::new()?;
     let output = temp_dir.child("count.txt.gz");
 
@@ -107,5 +107,54 @@ fn count_write_output_gzip() -> TestResult {
     assert_eq!(actual, "1\n");
 
     temp_dir.close()?;
+    Ok(())
+}
+
+#[test]
+fn count_where() -> TestResult {
+    let mut cmd = marc21_cmd();
+    let assert = cmd
+        .args(["count", "-s"])
+        .arg(data_dir().join("DUMP.mrc.gz"))
+        .args(["--where", "075{ b == 'p' && 2 == 'gndgen' }"])
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::ord::eq("4\n"))
+        .stderr(predicates::str::is_empty());
+
+    Ok(())
+}
+
+#[test]
+fn count_skip_invalid() -> TestResult {
+    let mut cmd = marc21_cmd();
+    let assert = cmd
+        .arg("count")
+        .arg(data_dir().join("invalid.mrc"))
+        .assert();
+
+    assert
+        .failure()
+        .code(1)
+        .stdout(predicates::str::is_empty())
+        .stderr(predicates::str::starts_with(
+            "error: could not parse record 0",
+        ));
+
+    let mut cmd = marc21_cmd();
+    let assert = cmd
+        .args(["count", "-s"])
+        .arg(data_dir().join("invalid.mrc"))
+        .assert();
+
+    assert
+        .success()
+        .code(0)
+        .stdout(predicates::ord::eq("0\n"))
+        .stderr(predicates::str::is_empty());
+
     Ok(())
 }
