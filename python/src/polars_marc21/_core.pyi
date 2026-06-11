497012241736988479fb40fa782eecbe59e752d9
Nico Wagner <n.wagner@dnb.de>
2026-06-11 09:27:27 +0200

    python: Enable all `ruff` rules  (#161)
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- python/src/polars_marc21/_core.pyi
+++ python/src/polars_marc21/_core.pyi
@@ -1,8 +1,8 @@
 from typing import Iterator
 from pathlib import Path
 
+
 class LazyReader(Iterator[list[str]]):
-    def __init__(self, paths: list[Path], query: str) -> None:...
+    def __init__(self, paths: list[Path], query: str) -> None: ...
 
     def arity(self) -> int: ...
-
--- python/tests/test_scan_marc21.py
+++ python/tests/test_scan_marc21.py
@@ -1,10 +1,15 @@
-from polars_marc21 import scan_marc21
+"""Checks of `scan_marc21` function."""
 
 import polars as pl
 from polars.testing import assert_frame_equal
 
+from polars_marc21 import scan_marc21
 
-def test_scan_marc21():
+__all__ = []
+
+
+def test_scan_marc21() -> None:
+    """Simple `scan_marc21` smoke test."""
     lhs = scan_marc21(
         "../tests/data/DUMP.mrc.gz",
         query="001, 075{ b | 2 == 'gndgen' }",
@@ -23,7 +28,7 @@ def test_scan_marc21():
                 "040993396",
             ],
             "gndgen": list("ppppuuu"),
-        }
+        },
     ).lazy()
 
     assert_frame_equal(lhs, rhs)
