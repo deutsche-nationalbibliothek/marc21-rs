dc4cdf33afb009a31dc6a6361e24a72794e78ccb
Nico Wagner <n.wagner@dnb.de>
2026-07-01 09:38:19 +0200

    python: add `read_marc21` function
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

--- python/src/polars_marc21/__init__.py
+++ python/src/polars_marc21/__init__.py
@@ -2,13 +2,20 @@
 Polars io plugin for reading MARC21 records.
 
 The packages provides `scan_marc21` which project MARC21 records to a
-DataFrame. Currently, all columns are returned as strings and may need
-to be converted to the required data type afterward
+LazyFrame. Currently, all columns are returned as strings and may need
+to be converted to the required data type afterward. In addition, the
+`read_marc21` function is provided, which returns a DataFrame instead of
+a LazyFrame.
 """
 
-from polars_marc21._scan import HeaderLengthError, scan_marc21
+from polars_marc21._scan import (
+    HeaderLengthError,
+    read_marc21,
+    scan_marc21,
+)
 
 __all__ = (
     "HeaderLengthError",
+    "read_marc21",
     "scan_marc21",
 )
