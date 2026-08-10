55d7208541b5bd6e6a7d67385253d474e6ccaf8e
Nico Wagner <n.wagner@dnb.de>
2026-08-10 11:33:38 +0200

    python: add `marc21_select` and `marc21_query` functions (#322)
    Signed-off-by: Nico Wagner <n.wagner@dnb.de>

---
+++ python/src/polars_marc21/_builder.py
@@ -0,0 +1,70 @@
+from pathlib import Path
+from typing import Self
+
+from polars import DataFrame, LazyFrame
+
+from ._scan import scan_marc21
+
+
+class QueryBuilder:
+    _query: str  = ""
+    _sources: str | Path | list[str] | list[Path]
+    _predicate: str | None = None
+    _header: str | list[str] | None = None
+
+    def __init__(self) -> None:
+        self._sources = []
+
+    def select(self, query: str) -> Self:
+        self._query = query
+        return self
+
+    def from_(
+        self,
+        sources: str | Path | list[str] | list[Path],
+    ) -> Self:
+        self._sources = sources
+        return self
+
+    def where(self, predicate: str) -> Self:
+        if self._predicate:
+            self._predicate = f"({self._predicate}) && " + predicate
+        else:
+            self._predicate = predicate
+
+        return self
+
+    def and_(self, predicate: str) -> Self:
+        self.where(predicate)
+        return self
+
+    def or_(self, predicate: str) -> Self:
+        if self._predicate:
+            self._predicate = f"({self._predicate}) || " + predicate
+        else:
+            self._predicate = predicate
+
+        return self
+
+    def header(self, header: str | list[str]) -> Self:
+        self._header = header
+        return self
+
+    def scan(self) -> LazyFrame:
+        return scan_marc21(
+            self._sources,
+            self._query,
+            header=self._header,
+            where=self._predicate,
+        )
+
+    def collect(self) -> DataFrame:
+        return self.scan().collect()
+
+
+def marc21_query() -> QueryBuilder:
+    return QueryBuilder()
+
+
+def marc21_select(query: str) -> QueryBuilder:
+    return QueryBuilder().select(query)
